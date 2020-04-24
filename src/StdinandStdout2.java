f83ca71c8fff18647e5c1f14fbd4c3b1581e4a00
SriVinayA <srivinay.appari@gmail.com>
2020-04-24 14:33:14 +0530

    minor changes

--- src/StdinandStdout2.java
+++ src/StdinandStdout2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StdinandStdoutII {
+public class StdinandStdout2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
