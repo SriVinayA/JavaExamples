f892a2d2d2b94eaf401beef0417625079c52f4e8
SriVinayA <srivinay.appari@gmail.com>
2020-04-24 14:43:47 +0530

    files uploaded

--- src/Loops_1.java
+++ src/Loops_1.java
@@ -11,6 +11,10 @@ class Loop_1 {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
+        
+        for (int i = 1; i < 11; i++) {
+            System.out.printf("%d x %d = %d%n", N, i, N*i);
+        }
 
     }
 }
