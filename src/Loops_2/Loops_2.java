64bd1ed23a67efbfd7f109235963c32d5a78531f
SriVinayA <srivinay.appari@gmail.com>
2020-04-26 18:43:52 +0530

    minor changes

--- src/Loops_2/Loops_2.java
+++ src/Loops_2/Loops_2.java
@@ -1,26 +1,15 @@
-package Loops_2;
+import java.util.*;
+import java.io.*;
 
-import java.util.Scanner;
-
-public class Loops_2 {
-    private static int n;
-
+class Loops_2{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
         for(int i=0;i<t;i++){
             int a = in.nextInt();
             int b = in.nextInt();
-            n = in.nextInt();
+            int n = in.nextInt();
         }
         in.close();
-
-        for (int i = 0; i < t; i++) {
-            for (int j = 0; j < n; j++) {
-                
-            }
-            
-        }
-
     }
 }
