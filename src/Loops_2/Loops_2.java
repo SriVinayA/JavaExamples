9d41f1abcd9225dd50f4add55f016a8ebf5655c8
SriVinayA <srivinay.appari@gmail.com>
2020-04-26 21:57:30 +0530

    minor changes

--- src/Loops_2/Loops_2.java
+++ src/Loops_2/Loops_2.java
@@ -1,25 +1,30 @@
 package Loops_2;
 
 import java.util.*;
-import java.io.*;
 
 public class Loops_2 {
     public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        int s = 0;
+
         for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
 
+            int s = 0;
             for (int j = 0; j < n; j++) {
                 if (j == 0) {
                     s = a + b;
                 } else {
                     s += (Math.pow(2, j)) * b;
                 }
-                System.out.format("%d ", s);
+
+                if (j == n - 1) {
+                    System.out.format("%d%n", s);
+                } else {
+                    //System.out.format("%d ", s);
+                }
             }
         }
         in.close();
