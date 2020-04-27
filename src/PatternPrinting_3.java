938ac381fb65b2d472cc38fba80f8cb8822526a6
SriVinayA <srivinay.appari@gmail.com>
2020-04-28 00:01:18 +0530

    minor changes

---
+++ src/PatternPrinting_3.java
@@ -0,0 +1,33 @@
+/*
+
+         1
+        121
+       12321
+      1234321
+     123454321
+    12345654321
+   1234567654321
+  123456787654321
+ 12345678987654321
+12345678910987654321
+
+ */
+
+public class PatternPrinting_3 {
+    public static void main(String[] args) {
+        int n = 10;
+
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 0; spaces < n - row; spaces++) {
+                System.out.print(" ");
+            }
+            for (int inc = 1; inc <= row; inc++) {
+                System.out.print(inc);
+            }
+            for (int dec = row - 1; dec >= 1; dec--) {
+                System.out.print(dec);
+            }
+            System.out.println();
+        }
+    }
+}
