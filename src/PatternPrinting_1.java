0aae4a30b03780017d8bb1a9a4b38b3473238175
SriVinayA <srivinay.appari@gmail.com>
2020-04-27 23:26:27 +0530

    file uploaded

--- src/PatternPrinting_1.java
+++ src/PatternPrinting_1.java
@@ -19,7 +19,7 @@ public class PatternPrinting_1 {
     public static void main(String[] args) {
         int n = 10;
 
-        for (int row = 1; row <= 10; row++) {
+        for (int row = 1; row <= n; row++) {
             for (int num = 1; num <= row; num++) {
                 System.out.print(num);
             }
