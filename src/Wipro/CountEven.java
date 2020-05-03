7f3203c3eec08081ba0f66974521e82de71b8e61
SriVinayA <srivinay.appari@gmail.com>
2020-05-04 00:16:27 +0530

    file added

--- src/Wipro/CountEven.java
+++ src/Wipro/CountEven.java
@@ -2,17 +2,17 @@ package Wipro;
 
 public class CountEven {
     public static void main(String[] args) {
-        int intput1 = 12;
-        int intput2 = 17;
-        int intput3 = 19;
-        int intput4 = 14;
-        int intput5 = 115;
+        int input1 = 12;
+        int input2 = 17;
+        int input3 = 19;
+        int input4 = 14;
+        int input5 = 115;
 
-        int[] intput = { intput1, intput2, intput3, intput4, intput5 };
+        int[] input = { input1, input2, input3, input4, input5 };
         int num = 0;
 
-        for (int i = 0; i < intput.length; i++) {
-            if (intput[i] % 2 == 0) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 == 0) {
                 num++;
             }
         }
---
+++ src/Wipro/CountEvenOdd.java
@@ -0,0 +1,26 @@
+package Wipro;
+
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        int input1 = 0;
+        int input2 = 17;
+        int input3 = 19;
+        int input4 = 14;
+        int input5 = 115;
+        String input6 = "even";
+
+        int[] input = { input1, input2, input3, input4, input5 };
+        int count = 0;
+        int len = input.length;
+        for (int i = 0; i < len; i++) {
+            if (input[i] % 2 == 0) {
+                count++;
+            }
+        }
+        if (input6 == "even") {
+            System.out.println(count);
+        } else {
+            System.out.println(len - count);
+        }
+    }
+}
