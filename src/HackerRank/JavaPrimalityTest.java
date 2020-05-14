fc5922ec110e09d3e0f95097367ad9649eb752e8
SriVinayA <srivinay.appari@gmail.com>
2020-05-14 13:42:39 +0530

    file added

---
+++ src/HackerRank/JavaPrimalityTest.java
@@ -0,0 +1,11 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class JavaPrimalityTest {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in);) {
+            System.out.println(sc.nextBigInteger().isProbablePrime(100) ? "prime" : "not prime");
+        }
+    }
+}
