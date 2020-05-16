3cd97c028d372017b198323ffabbf601cac49720
SriVinayA <srivinay.appari@gmail.com>
2020-05-17 01:48:20 +0530

    files added

--- src/AddingElementsInAnArray.java
+++ src/AddingElementsInAnArray.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 public class AddingElementsInAnArray {
     public static void main(String[] args) {
+        Integer arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int x = 50;
 
-    int[] arr = {12, 13, 14};
-    List arr1 = Arrays.asList(arr);
-    System.out.println(arr1);
-    arr1.add(15);
-    arr1.add(16);
-    System.out.println(arr1);
-    }   
+        System.out.println("Initial Array:\n" + Arrays.toString(arr));
+
+        List<Integer> arrlist = new ArrayList<Integer>(Arrays.asList(arr));
+        arrlist.add(x);
+        arr = arrlist.toArray(arr);
+
+        System.out.println("\nArray with " + x + " added:\n" + Arrays.toString(arr));
+    }
 }
