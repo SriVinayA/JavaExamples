31deafbc2ed013ed46e3d5e82827d882bf565ec3
SriVinayA <srivinay.appari@gmail.com>
2020-05-20 17:09:49 +0530

    minor changes

--- src/Wipro/WeightOfTheHillPattern.java
+++ src/Wipro/WeightOfTheHillPattern.java
@@ -2,16 +2,15 @@ package Wipro;
 
 public class WeightOfTheHillPattern {
     public static void main(String[] args) {
-        int input1 = 5;
-        int input2 = 10;
-        int input3 = 2;
-        int count = 0;
-        for (int i = 0; i < input1; i++) {
-            for (int j = 0; j <= i; j++) {
-                count = input2 + count;
-            }
-            input2 = input2 + input3;
+        int input1 = 4;
+        int input2 = 1;
+        int input3 = 5;
+        int sum = 0;
+
+        for (int i = 1; i <= input1; i++) {
+            sum += (input2 * i);
+            input2 += input3;
         }
-        System.out.println(count);
+        System.out.println(sum);
     }
 }
