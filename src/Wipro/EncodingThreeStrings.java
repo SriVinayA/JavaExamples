052a6d3f66a6f782d7398b10f3482f001b56e494
SriVinayA <srivinay.appari@gmail.com>
2020-06-13 13:34:45 +0530

    file added

--- src/Wipro/EncodingThreeStrings.java
+++ src/Wipro/EncodingThreeStrings.java
@@ -1,45 +1,39 @@
 package Wipro;
 
-import java.util.ArrayList;
-
 public class EncodingThreeStrings {
     public static void main(String[] args) {
         String input1 = "abc";
-        String input2 = "abcd";
-        String input3 = "abcde";
+        String input2 = "wxyz";
+        String input3 = "mnopq";
 
-        int len1 = input1.length(), len2 = input2.length(), len3 = input3.length();
-        ArrayList<String> arr1 = new ArrayList<>();
-        ArrayList<String> arr2 = new ArrayList<>();
-        ArrayList<String> arr3 = new ArrayList<>();
+        String[] arr1 = new String[3];
+        String[] arr2 = new String[3];
+        String[] arr3 = new String[3];
 
-        stringDivision(input1, len1, arr1);
+        stringDivision(input1, input1.length(), arr1);
+        stringDivision(input2, input2.length(), arr2);
+        stringDivision(input3, input3.length(), arr3);
 
-        stringDivision(input2, len2, arr2);
+        System.out.println(arr1[0] + arr2[0] + arr3[0]);
+        System.out.println(arr1[1] + arr2[1] + arr3[1]);
+        System.out.println((arr1[2] + arr2[2] + arr3[2]).toUpperCase());
 
-        stringDivision(input3, len3, arr3);
-
-        // System.out.println(arr1.get(0) + arr2.get(0) + arr3.get(0));
-        // System.out.println(arr1.get(1) + arr2.get(1) + arr3.get(1));
-        // System.out.println((arr1.get(2) + arr2.get(2) + arr3.get(2)).toUpperCase());
     }
 
-    private static void stringDivision(String input, int len, ArrayList<String> arr) {
-        if ((len - 3) % 3 == 0) {
-            int k = (len - 3) / 3;
-            arr.add(input.substring(0, k + 1));
-            arr.add(input.substring(k + 1, (2 * k) + 2));
-            arr.add(input.substring((2 * k) + 2, (3 * k) + 3));
-        } else if ((len - 4) % 3 != 0) {
-            int k = (len - 4) / 3;
-            arr.add(input.substring(0, k + 1));
-            arr.add(input.substring(k + 1, (2 * k) + 3));
-            arr.add(input.substring((2 * k) + 3, (3 * k) + 4));
-        } else if ((len - 5) % 3 == 0) {
-            int k = (len - 5) / 3;
-            arr.add(input.substring(0, k + 2));
-            arr.add(input.substring(k + 2, (2 * k) + 3));
-            arr.add(input.substring((2 * k) + 3, (2 * k) + 7));
+    private static void stringDivision(String input, int len, String[] arr) {
+        int k = len / 3;
+        if (len % 3 == 0) {
+            arr[0] = input.substring(0, k);
+            arr[1] = input.substring(k, 2 * k);
+            arr[2] = input.substring(2 * k, len);
+        } else if (len % 3 == 1) {
+            arr[0] = input.substring(0, k);
+            arr[1] = input.substring(k, 2 * k + 1);
+            arr[2] = input.substring(2 * k + 1, len);
+        } else if (len % 3 == 2) {
+            arr[0] = input.substring(0, k + 1);
+            arr[1] = input.substring(k + 1, 2 * k + 1);
+            arr[2] = input.substring(2 * k + 1, len);
         }
     }
 }
