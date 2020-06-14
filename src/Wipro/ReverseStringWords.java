f7b012ba7f671c30614a5b02ed6cf744b71e9965
SriVinayA <srivinay.appari@gmail.com>
2020-06-15 00:44:37 +0530

    file added

--- src/Wipro/ReverseStringWords.java
+++ src/Wipro/ReverseStringWords.java
@@ -1,33 +1,85 @@
 /*
  * input2 = 0 
  * ---------- 
- * Wipro Technologies Bangalore -> orpiW seigolonhceT erolagnaB
+ * Wipro TechNologies BangaLore -> orpiW seigoloNhceT eroLagnaB
  * 
- * Wipro Technologies, Bangalore -> orpiW ,seigolonhceT erolagnaB
- * 
+ * Wipro TechNologies, BangaLore -> orpiW ,seigoloNhceT eroLagnaB
+ *
  * input2 = 1 
  * ---------- 
- * Wipro Technologies Bangalore -> Orpiw Seigolonhcet Erolagnab
+ * Wipro TechNologies BangaLore -> Orpiw SeigOlonhcet ErolaGnab
  * 
- * Wipro Technologies, Bangalore -> Orpiw ,seigolonhceT Erolagnab
+ * Wipro TechNologies, BangaLore -> Orpiw ,seigoloNhceT ErolaGnab
  */
 
 package Wipro;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ReverseStringWords {
     public static void main(String[] args) {
-        String input1 = "Wipro Technologies Bangalore";
+        String input1 = "Wipro TechNologies, BangaLore";
         Random random = new Random();
-        int input2 = random.nextInt(1);
+        int input2 = random.nextInt(2); // -> 0 or 1
 
+        StringBuilder result = new StringBuilder();
         String[] arr = input1.split(" ");
-        if(input2 == 0){
-            for (String s : arr) {
 
+        if (input2 == 0) {
+            for (String s : arr) {
+                String str = reverseWord(s);
+                result.append(str).append(" ");
             }
         }
+
+        else if (input2 == 1) {
+            for (String s : arr) {
+                StringBuilder word = new StringBuilder();
+                ArrayList<Integer> al = new ArrayList<>();
+                for (int i = 0; i < s.length(); i++) {
+                    int num = s.charAt(i) - 64;
+                    if (1 < num && num < 26) {
+                        al.add(i);
+                    }
+                }
+                String str = reverseWord(s.toLowerCase());
+                for (int i = 0; i < str.length(); i++) {
+                    boolean k = checkNumber(i, al);
+                    if (k) {
+                        if (Character.isLetter(str.charAt(i))) {
+                            word.append(Character.toUpperCase(str.charAt(i)));
+                        } else {
+                            word = new StringBuilder(reverseWord(s));
+                            break;
+                        }
+                    } else {
+                        word.append(str.charAt(i));
+                    }
+
+                }
+                result.append(word).append(" ");
+            }
+        }
+
+        System.out.println("input2 = " + input2);
+        System.out.println("----------");
+        System.out.println(result.toString().replaceAll(".$", ""));
+    }
+
+    private static boolean checkNumber(int i, ArrayList<Integer> al) {
+        boolean k = false;
+        for (int j : al) {
+            if (i == j) {
+                k = true;
+                break;
+            }
+        }
+        return k;
+    }
+
+    private static String reverseWord(String s) {
+        StringBuilder str = new StringBuilder(s);
+        return String.valueOf(str.reverse());
     }
 }
