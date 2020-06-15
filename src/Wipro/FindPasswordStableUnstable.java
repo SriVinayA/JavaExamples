ffcc6ab79b262492aca597b5feab8a9fa1923164
SriVinayA <srivinay.appari@gmail.com>
2020-06-15 23:48:12 +0530

    files added

--- src/Wipro/FindPasswordStableUnstable.java
+++ src/Wipro/FindPasswordStableUnstable.java
@@ -12,14 +12,42 @@ public class FindPasswordStableUnstable {
     public static void main(String[] args) {
         int input1 = 12, input2 = 1313, input3 = 122, input4 = 678, input5 = 898;
         int[] arr = { input1, input2, input3, input4, input5 };
+        int stable = 0;
+        int unstable = 0;
+
         for (int i : arr) {
             if (checkStable(i)) {
-                System.out.println(i);
+                stable += i;
+            } else{
+                unstable += i;
             }
         }
+        System.out.println(stable - unstable);
     }
 
-    private static boolean checkStable(int i) {
-        return false;
+    private static boolean checkStable(int input) {
+        String str = String.valueOf(input);
+        int num = 0;
+        boolean isStable = true;
+        int[] arr1 = new int[10];
+
+        for (int i = 0; i < str.length(); i++) {
+            arr1[Character.getNumericValue(str.charAt(i))]++;
+        }
+
+        for (int value : arr1) {
+            if (value > 0) {
+                num = value;
+                break;
+            }
+        }
+
+        for (int value : arr1) {
+            if (value != 0 && value != num) {
+                isStable = false;
+                break;
+            }
+        }
+        return isStable;
     }
 }
