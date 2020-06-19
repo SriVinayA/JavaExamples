fd0e7ef09842ea3257261691bbc9c63f687095cf
SriVinayA <srivinay.appari@gmail.com>
2020-06-20 01:24:48 +0530

    file added

--- src/Wipro/UserIDGeneration.java
+++ src/Wipro/UserIDGeneration.java
@@ -1,26 +1,26 @@
 /*
-String First_Name = "Rajiv"; 
-String Last_Name = "Roy"; 
-int PIN = 560037; 
-int N = 6;
+String input1 = "Rajiv"; 
+String input2 = "Roy"; 
+int input3 = 560037; 
+int input4 = 6;
 ----------------------------------
 ANSWER = YrAJIV75
 ----------------------------------
 
-String First_Name = "Manoj";
-String Last_Name = "Kumar";
-int PIN = 561327;
-int N = 2;
+String input1 = "Manoj";
+String input2 = "Kumar";
+int input3 = 561327;
+int input4 = 2;
 ----------------------------------
 ANSWER = RmANOJ62
 ----------------------------------
 
-String First_Name = "Kumad";
-String Last_Name = "Kumar";
-int PIN = 561327;
-int N = 2;
+String input1 = "Kumud";
+String input2 = "Kumar";
+int input3 = 561327;
+int input4 = 2;
 ----------------------------------
-ANSWER = RkUMAD62
+ANSWER = RkUMUD62
 ----------------------------------
  */
 
@@ -28,7 +28,7 @@ package Wipro;
 
 public class UserIDGeneration {
     public static void main(String[] args) {
-        String input1 = "Kumau";
+        String input1 = "Kumud";
         String input2 = "Kumar";
         int input3 = 561327;
         int input4 = 2;
@@ -52,6 +52,8 @@ public class UserIDGeneration {
             } else if (input1.compareTo(input2) > 0) {
                 smallerName = input2;
                 longerName = input1;
+            } else {
+                smallerName = longerName = input1;
             }
         }
 
@@ -60,9 +62,9 @@ public class UserIDGeneration {
         System.out.println(oppositeCase(str));
     }
 
-    private static String pinCode(int pin, int N) {
+    private static String pinCode(int pin, int input4) {
         String str = String.valueOf(pin);
-        return "" + str.charAt(N - 1) + str.charAt(str.length() - N);
+        return "" + str.charAt(input4 - 1) + str.charAt(str.length() - input4);
     }
 
     private static String oppositeCase(String str) {
