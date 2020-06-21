57254cfdcc5c912b293c986cbd057b1214acf4ca
SriVinayA <srivinay.appari@gmail.com>
2020-06-21 17:25:02 +0530

    oop

--- src/Oop/Employee.java
+++ src/Oop/Employee.java
@@ -1,10 +1,30 @@
 package Oop;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
-        return baseSalary + (hourlyRate * extraHours);
+        return baseSalary + (getHourlyRate() * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less.");
+        this.baseSalary = baseSalary;
+    }
+
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or negative");
+        this.hourlyRate = hourlyRate;
     }
 }
