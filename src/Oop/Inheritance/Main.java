828cd72f887bef919a9cae9e47c319ed59c9e9ed
SriVinayA <srivinay.appari@gmail.com>
2020-07-03 20:44:55 +0530

    oop

--- src/Oop/Inheritance/Main.java
+++ src/Oop/Inheritance/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         var point1 = new Point(1, 2);
         var point2 = new Point(1, 2);
-        System.out.println(point1 == point2);
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
     }
 }
