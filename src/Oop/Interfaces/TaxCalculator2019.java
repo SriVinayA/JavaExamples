72196e3c0124313bac78b98cd039e1a5439a03ad
SriVinayA <srivinay.appari@gmail.com>
2020-07-05 21:11:32 +0530

    oop

--- src/Oop/Interfaces/TaxCalculator2019.java
+++ src/Oop/Interfaces/TaxCalculator2019.java
@@ -1,4 +1,8 @@
 package Oop.Interfaces;
 
-public class TaxCalculator2019 {
+public class TaxCalculator2019 implements TaxCalculator{
+    @Override
+    public double calculateTax() {
+        return 0;
+    }
 }
