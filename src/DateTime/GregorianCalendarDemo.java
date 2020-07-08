44e4bf6fe60422918da2ba5f22c8898b789f4965
SriVinayA <srivinay.appari@gmail.com>
2020-07-08 23:17:24 +0530

    Date & Time

---
+++ src/DateTime/GregorianCalendarDemo.java
@@ -0,0 +1,29 @@
+package DateTime;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class GregorianCalendarDemo {
+    public static void main(String[] args) {
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        int year = 0;
+
+        var calendar = new GregorianCalendar();
+
+        // Display current time and date information.
+        System.out.println("Date: " +
+                months[calendar.get(Calendar.MONTH)] +
+                " " + calendar.get(Calendar.DATE) + " " +
+                calendar.get(Calendar.YEAR));
+        System.out.println("Time: " +
+                calendar.get(Calendar.HOUR) + ":" +
+                calendar.get(Calendar.MINUTE) + ":" +
+                calendar.get(Calendar.SECOND));
+
+        // Test if the current year is a leap year
+        if(calendar.isLeapYear(year))
+            System.out.println("The current year is a leap year");
+        else
+            System.out.println("The current year is not a leap year");
+    }
+}
