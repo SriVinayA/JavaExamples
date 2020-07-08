36ce46e30e50a4da361d79dcf701116584743bf3
SriVinayA <srivinay.appari@gmail.com>
2020-07-08 21:42:46 +0530

    Date & Time

---
+++ src/DateTime/GettingCurrentDateAndTime.java
@@ -0,0 +1,11 @@
+package DateTime;
+
+import java.util.Date;
+
+public class GettingCurrentDateAndTime {
+    public static void main(String[] args) {
+        var date = new Date();
+
+        System.out.println(date.toString());
+    }
+}
