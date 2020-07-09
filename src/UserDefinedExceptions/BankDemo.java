057c0ee4b33488d203479f22eb7218c7054ca09b
SriVinayA <srivinay.appari@gmail.com>
2020-07-10 00:21:26 +0530

    exceptions

--- src/UserDefinedExceptions/BankDemo.java
+++ src/UserDefinedExceptions/BankDemo.java
@@ -2,5 +2,18 @@ package UserDefinedExceptions;
 
 public class BankDemo {
     public static void main(String[] args) {
+        CheckingAccount account = new CheckingAccount(101);
+        System.out.println("Depositing $500...");
+        account.deposit(500.00);
+
+        try {
+            System.out.println("\nWithdrawing $100...");
+            account.withdraw(100.00);
+            System.out.println("\nWithdrawing $600...");
+            account.withdraw(600.00);
+        }catch (InsufficientFundsException e) {
+            System.out.println("Sorry, but you are short $" + e.getAmount());
+            e.printStackTrace();
+        }
     }
 }
