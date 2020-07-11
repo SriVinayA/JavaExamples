123c7304344b5726adbf69532e2908452be34358
SriVinayA <srivinay.appari@gmail.com>
2020-07-11 17:00:46 +0530

    files added

---
+++ src/Inheritance_Examples/Calculation.java
@@ -0,0 +1,15 @@
+package Inheritance_Examples;
+
+public class Calculation {
+    private int z;
+
+    public void addition (int x, int y) {
+        z = x + y;
+        System.out.println("The sum of the given numbers:"+z);
+    }
+
+    public void subtraction (int x, int y) {
+        z = x - y;
+        System.out.println("The difference between the given numbers:"+z);
+    }
+}
