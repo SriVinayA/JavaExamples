3139d34d297f5724d9be96ca56fb752376eb5924
SriVinayA <srivinay.appari@gmail.com>
2020-07-11 21:30:31 +0530

    Overriding

---
+++ src/JavaOveriding/TestDog.java
@@ -0,0 +1,23 @@
+package JavaOveriding;
+
+class Animal {
+    public void move() {
+        System.out.println("Animals can move");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void move() {
+        super.move();
+        System.out.println("Dogs can walk and run");
+
+    }
+}
+
+public class TestDog {
+    public static void main(String[] args) {
+        Animal b = new Dog();   // Animal reference but Dog object
+        b.move();   // runs the method in Dog class
+    }
+}
