dde894b82ccbfdddc54eddc5b98d52ce2d8de7f3
SriVinayA <srivinay.appari@gmail.com>
2020-07-12 18:29:05 +0530

    abstraction

--- src/Abstraction_Examples/AbstractClassExample.java
+++ src/Abstraction_Examples/AbstractClassExample.java
@@ -1,24 +1,4 @@
 package Abstraction_Examples;
 
-abstract class Shape{
-    String color;
-
-    // these are abstract methods
-    abstract double area();
-
-    @Override
-    public abstract String toString();
-
-    public Shape(String color) {
-        System.out.println("Abstraction_Examples.Shape constructor called");
-        this.color = color;
-    }
-
-    // this is a concrete method
-    public String getColor() {
-        return color;
-    }
-}
-
 public class AbstractClassExample {
 }
