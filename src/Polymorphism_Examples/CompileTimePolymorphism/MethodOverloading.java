9671d8dfcdd7d94d4abd86345233441fba1b9810
SriVinayA <srivinay.appari@gmail.com>
2020-07-12 17:07:36 +0530

    polymorphmism

--- src/Polymorphism_Examples/CompileTimePolymorphism/MethodOverloading.java
+++ src/Polymorphism_Examples/CompileTimePolymorphism/MethodOverloading.java
@@ -1,4 +1,23 @@
 package Polymorphism_Examples.CompileTimePolymorphism;
 
-public class MethodOverloading {
+class MultiplyFun {
+
+    // Method with 2 parameter
+    static int Multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method with the same name but 2 double parameter
+    static double Multiply(double a, double b) {
+        return a * b;
+    }
+}
+
+class MethodOverloading {
+    public static void main(String[] args) {
+
+        System.out.println(MultiplyFun.Multiply(2, 4));
+
+        System.out.println(MultiplyFun.Multiply(5.5, 6.3));
+    }
 }
