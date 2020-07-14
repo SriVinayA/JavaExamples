6c09f2bc16b8d9bdf0f5bf0f2dfb9e89fd6ea15f
SriVinayA <srivinay.appari@gmail.com>
2020-07-14 22:48:43 +0530

    interfaces

--- src/Interfaces_Examples/Example2/Main.java
+++ src/Interfaces_Examples/Example2/Main.java
@@ -1,4 +1,38 @@
 package Interfaces_Examples.Example2;
 
-public class Main {
+public class Main implements Hockey{
+    public static void main(String[] args) {
+        var m = new Main();
+        //m.
+    }
+
+    @Override
+    public void homeGoalScored() {
+
+    }
+
+    @Override
+    public void visitingGoalScored() {
+
+    }
+
+    @Override
+    public void endOfPeriod(int period) {
+
+    }
+
+    @Override
+    public void overtimePeriod(int ot) {
+
+    }
+
+    @Override
+    public void setHomeTeam(String name) {
+
+    }
+
+    @Override
+    public void setVisitingTeam(String name) {
+
+    }
 }
