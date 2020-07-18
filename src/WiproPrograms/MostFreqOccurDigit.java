5660c0a25b71f65b552e15ed245852865509d090
SriVinayA <srivinay.appari@gmail.com>
2020-07-18 21:46:42 +0530

    java8

--- src/WiproPrograms/MostFreqOccurDigit.java
+++ src/WiproPrograms/MostFreqOccurDigit.java
@@ -2,21 +2,21 @@ package WiproPrograms;
 
 public class MostFreqOccurDigit {
 	public static void main(String[] args) {
-		int[] input1 = { 1237, 202, 666, 140 }; // 6
-		// int input2 = input1.length;
+		int input1 = 123, input2 = 456, input3 = 345, input4 = 5043; //6
 
+		int[] arr = {input1, input2, input3, input4};
 		int output = 0;
-		int[] arr = new int[10];
+		int[] count = new int[10];
 		int max = 0;
-		for (int k : input1) {
-			String str = String.valueOf(k);
+		for (int i : arr) {
+			String str = String.valueOf(i);
 			for (int j = 0; j < str.length(); j++) {
 				int n = Character.getNumericValue(str.charAt(j));
-				arr[n] += 1;
+				count[n]++;
 			}
 		}
-		for (int i = 0; i < arr.length; i++) {
-			int a = arr[i];
+		for (int i = 0; i < count.length; i++) {
+			int a = count[i];
 			if (max <= a) {
 				max = a;
 				output = i;
