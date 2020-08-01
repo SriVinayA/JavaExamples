ed3ca48b5737b848fc4d47f0bbafcb332e88ddad
SriVinayA <srivinay.appari@gmail.com>
2020-08-01 13:14:56 +0530

    minor changes

--- src/BasicPrograms/AddingElementsInAnArray.java
+++ src/BasicPrograms/AddingElementsInAnArray.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package BasicPrograms;
 
 import java.util.*;
@@ -16,23 +15,4 @@ public class AddingElementsInAnArray {
 
         System.out.println("\nArray with " + x + " added:\n" + Arrays.toString(arr));
     }
-=======
-package BasicPrograms;
-
-import java.util.*;
-
-public class AddingElementsInAnArray {
-    public static void main(String[] args) {
-        Integer[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        int x = 50;
-
-        System.out.println("Initial Array:\n" + Arrays.toString(arr));
-
-        List<Integer> arrList = new ArrayList<>(Arrays.asList(arr));
-        arrList.add(x);
-        arr = arrList.toArray(arr);
-
-        System.out.println("\nArray with " + x + " added:\n" + Arrays.toString(arr));
-    }
->>>>>>> e0f541b2bfb549282a1ad231a350b5847dc8c7aa
 }
