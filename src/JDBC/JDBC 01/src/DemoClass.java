764066738a855a2639ffef8d1312f7398ff8dbb3
Appari Sri Vinay <srivinay.appari@gmail.com>
2020-08-18 00:17:50 +0530

    jdbc

---
+++ src/JDBC/JDBC 01/src/DemoClass.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class DemoClass {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String url = "jdbc:mysql://localhost:3306/test";
+		String user = "root";
+		String password = "root";
+		String query = "select name from students where id=3";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		
+		rs.next();
+		String name = rs.getString("name");
+		
+		System.out.println(name);
+		
+		st.close();
+		con.close();
+		
+	}
+}
